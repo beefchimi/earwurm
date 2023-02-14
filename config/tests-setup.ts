c9a177738cc79df3f14b7a6268f93f567116d57f
beefchimi <curtdulmage@gmail.com>
2023-02-14 17:10:47 -0500

    :hammer_and_wrench: [Tests] Global stub AudioNode and AudioParam

--- config/tests-setup.ts
+++ config/tests-setup.ts
@@ -4,11 +4,17 @@ import {
   MockAudioBuffer,
   MockAudioBufferSourceNode,
   MockAudioContext,
+  MockAudioNode,
+  MockAudioParam,
 } from '../src/tests/mock';
 
+// We may need to stub more globals as we encounter
+// errors during testing.
 vi.stubGlobal('AudioBuffer', MockAudioBuffer);
 vi.stubGlobal('AudioBufferSourceNode', MockAudioBufferSourceNode);
 vi.stubGlobal('AudioContext', MockAudioContext);
+vi.stubGlobal('AudioNode', MockAudioNode);
+vi.stubGlobal('AudioParam', MockAudioParam);
 
 beforeEach(() => {
   vi.clearAllMocks();
