e9106fcdfdb1a368069e7840520ab2075255c78b
beefchimi <curtdulmage@gmail.com>
2023-02-24 12:14:01 -0500

    :art: [Sound] Rename some mock variables in tests

--- src/tests/Sound.test.ts
+++ src/tests/Sound.test.ts
@@ -16,7 +16,7 @@ describe('Sound component', () => {
   const defaultAudioNode = new AudioNode();
 
   describe('initialization', () => {
-    const mockSound = new Sound(
+    const testSound = new Sound(
       'TestInit',
       defaultAudioBuffer,
       defaultContext,
@@ -24,13 +24,13 @@ describe('Sound component', () => {
     );
 
     it('is initialized with default values', () => {
-      expect(mockSound).toBeInstanceOf(Sound);
+      expect(testSound).toBeInstanceOf(Sound);
 
-      expect(mockSound).toHaveProperty('volume', 1);
-      expect(mockSound).toHaveProperty('mute', false);
-      expect(mockSound).toHaveProperty('loop', false);
-      expect(mockSound).toHaveProperty('duration', 0);
-      expect(mockSound).toHaveProperty('state', 'created');
+      expect(testSound).toHaveProperty('volume', 1);
+      expect(testSound).toHaveProperty('mute', false);
+      expect(testSound).toHaveProperty('loop', false);
+      expect(testSound).toHaveProperty('duration', 0);
+      expect(testSound).toHaveProperty('state', 'created');
     });
   });
 
@@ -41,7 +41,7 @@ describe('Sound component', () => {
   // describe('volume', () => {});
 
   describe('loop', () => {
-    const mockSound = new Sound(
+    const testSound = new Sound(
       'TestLoop',
       defaultAudioBuffer,
       defaultContext,
@@ -50,14 +50,14 @@ describe('Sound component', () => {
 
     it('allows `set` and `get`', () => {
       // TODO: Should check `AudioBufferSourceNode` value.
-      expect(mockSound.loop).toBe(false);
-      mockSound.loop = true;
-      expect(mockSound.loop).toBe(true);
+      expect(testSound.loop).toBe(false);
+      testSound.loop = true;
+      expect(testSound.loop).toBe(true);
     });
   });
 
   describe('duration', () => {
-    const mockSound = new Sound(
+    const testSound = new Sound(
       'TestDuration',
       defaultAudioBuffer,
       defaultContext,
@@ -66,7 +66,7 @@ describe('Sound component', () => {
 
     it('allows `get`', () => {
       // TODO: We should provide a buffer that has a `duration`.
-      expect(mockSound.duration).toBe(0);
+      expect(testSound.duration).toBe(0);
     });
   });
 
@@ -82,26 +82,26 @@ describe('Sound component', () => {
     ];
 
     it('starts playing the source', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
       const spySourceStart = vi.spyOn(AudioBufferSourceNode.prototype, 'start');
 
       expect(spySourceStart).not.toBeCalled();
-      mockSound.play();
+      testSound.play();
       expect(spySourceStart).toBeCalledTimes(1);
     });
 
     it('updates state', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
 
-      mockSound.play();
-      expect(mockSound.state).toBe('playing');
+      testSound.play();
+      expect(testSound.state).toBe('playing');
     });
 
     it('returns instance', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
-      const instance = mockSound.play();
+      const testSound = new Sound(...mockConstructorArgs);
+      const instance = testSound.play();
 
-      expect(instance).toBe(mockSound);
+      expect(instance).toBe(testSound);
     });
   });
 
@@ -117,17 +117,17 @@ describe('Sound component', () => {
     it.todo('pauses the source');
 
     it('updates state', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
 
-      mockSound.play().pause();
-      expect(mockSound.state).toBe('paused');
+      testSound.play().pause();
+      expect(testSound.state).toBe('paused');
     });
 
     it('returns instance', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
-      const instance = mockSound.play().pause();
+      const testSound = new Sound(...mockConstructorArgs);
+      const instance = testSound.play().pause();
 
-      expect(instance).toBe(mockSound);
+      expect(instance).toBe(testSound);
     });
   });
 
@@ -140,45 +140,45 @@ describe('Sound component', () => {
     ];
 
     it('stops and disconnects the source', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
       const spySourceStop = vi.spyOn(AudioBufferSourceNode.prototype, 'stop');
       const spySourceDisconnect = vi.spyOn(
         AudioBufferSourceNode.prototype,
         'disconnect',
       );
 
-      mockSound.play();
+      testSound.play();
 
       expect(spySourceStop).not.toBeCalled();
       expect(spySourceDisconnect).not.toBeCalled();
-      mockSound.stop();
+      testSound.stop();
       expect(spySourceStop).toBeCalledTimes(1);
       expect(spySourceDisconnect).toBeCalledTimes(1);
     });
 
     it('updates state', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
 
-      mockSound.play().stop();
-      expect(mockSound.state).toBe('stopping');
+      testSound.play().stop();
+      expect(testSound.state).toBe('stopping');
     });
 
     it('empties active events', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
-      mockSound.on('ended', vi.fn());
+      const testSound = new Sound(...mockConstructorArgs);
+      testSound.on('ended', vi.fn());
 
       // Unable to spy on private `empty()` method, so we
       // check against `activeEvents` instead.
-      expect(mockSound.activeEvents).toHaveLength(1);
-      mockSound.play().stop();
-      expect(mockSound.activeEvents).toHaveLength(0);
+      expect(testSound.activeEvents).toHaveLength(1);
+      testSound.play().stop();
+      expect(testSound.activeEvents).toHaveLength(0);
     });
 
     it('returns instance', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
-      const instance = mockSound.play().stop();
+      const testSound = new Sound(...mockConstructorArgs);
+      const instance = testSound.play().stop();
 
-      expect(instance).toBe(mockSound);
+      expect(instance).toBe(testSound);
     });
   });
 
@@ -191,37 +191,37 @@ describe('Sound component', () => {
     ];
 
     it('emits an event for every statechange', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
       const spyStateChange: SoundEventMap['statechange'] = vi.fn(
         (_state) => {},
       );
 
-      mockSound.on('statechange', spyStateChange);
+      testSound.on('statechange', spyStateChange);
 
       expect(spyStateChange).not.toBeCalled();
 
-      mockSound.play();
+      testSound.play();
       expect(spyStateChange).toBeCalledWith('playing');
 
-      mockSound.pause();
+      testSound.pause();
       expect(spyStateChange).toBeCalledWith('paused');
 
-      mockSound.stop();
+      testSound.stop();
       expect(spyStateChange).toBeCalledWith('stopping');
     });
 
     it('emits `ended` event once sound has finished', () => {
-      const mockSound = new Sound(...mockConstructorArgs);
+      const testSound = new Sound(...mockConstructorArgs);
       const spyEnded: SoundEventMap['ended'] = vi.fn((_event) => {});
 
-      mockSound.on('ended', spyEnded);
-      mockSound.play();
+      testSound.on('ended', spyEnded);
+      testSound.play();
 
       expect(spyEnded).not.toBeCalled();
       vi.advanceTimersToNextTimer();
 
       expect(spyEnded).toBeCalledWith({
-        id: mockSound.id,
+        id: testSound.id,
         source: expect.any(AudioBufferSourceNode),
       });
     });
