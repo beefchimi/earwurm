d6c7a4a8d0f719087e7271099e95cf9257d68768
beefchimi <curtdulmage@gmail.com>
2023-02-24 10:05:24 -0500

    :hammer_and_wrench: [Vite] Fix output types

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,15 @@ export default defineConfig({
     },
     minify: false,
   },
-  plugins: [dts()],
+  plugins: [
+    dts({
+      // Will capture only the types that are exposed to consumers
+      // and condense them all into a single file. If we also want
+      // unexported types, as well as their folder structure, then
+      // replace `rollupTypes` with `insertTypesEntry`.
+      rollupTypes: true,
+    }),
+  ],
   test: {
     environment: 'happy-dom',
     setupFiles: 'config/tests-setup',
