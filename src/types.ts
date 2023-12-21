e7cd51fd6689b1092f792b4b823e9756d28abc3e
beefchimi <curtdulmage@gmail.com>
2023-12-21 12:11:00 -0500

    :label: [Types] Add PrimitiveType

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
+export type PrimitiveType =
+  | string
+  | number
+  | bigint
+  | boolean
+  | symbol
+  | undefined
+  | null;
+
 export type TimeoutId = number | ReturnType<typeof setTimeout>;
 
 // Tuple: custom error, original error.
