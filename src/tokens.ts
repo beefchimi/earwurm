d32a357e31318304e602ab89127dac5848b8100a
beefchimi <curtdulmage@gmail.com>
2023-12-29 12:42:15 -0500

    :speech_balloon: [Tokens] Clarify comment

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,6 +4,6 @@ export const tokens = {
   minStartTime: 0.0001,
   minSpeed: 0.25,
   maxSpeed: 4,
-  // Browser's don't seem to accept a `0` value.
+  // FireFox does not accept a `0` value.
   pauseSpeed: 0.0001,
 };
