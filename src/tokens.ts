2355ab6599fdf578cdbcfcd978e86d025326b3d1
beefchimi <curtdulmage@gmail.com>
2024-01-05 13:39:15 -0500

    :jigsaw: [Tokens] Update with static member values and new transitionSec

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,6 +1,15 @@
 export const tokens = {
-  maxStackSize: 8,
+  // Global
+  error: {
+    close: 'Failed to close the Earwurm AudioContext.',
+    resume: 'Failed to resume the Earwurm AudioContext.',
+  },
+  transitionSec: 0.2,
+  // Earwurm (manager)
   suspendAfterMs: 30000,
+  // Stack
+  maxStackSize: 8,
+  // Sound
   minStartTime: 0.0001,
   minSpeed: 0.25,
   maxSpeed: 4,
