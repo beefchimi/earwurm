c286c9fe5211f065f72b649551f1203a4102c4d0
beefchimi <curtdulmage@gmail.com>
2024-01-11 16:55:08 -0500

    :hammer_and_wrench: [Root] Ignore app folder in coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import {defineConfig} from 'vite';
+// import {configDefaults} from 'vitest/config';
 
 // Root-level config
 export default defineConfig({
   test: {
     environment: 'happy-dom',
     setupFiles: 'config/tests-setup',
+    // exclude: [...configDefaults.exclude, './app/**/*'],
+    coverage: {
+      exclude: ['app/**'],
+    },
   },
 });
