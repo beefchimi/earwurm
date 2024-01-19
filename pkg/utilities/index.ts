67b08352febffae7d0d8f8ea7c041a783813725b
beefchimi <curtdulmage@gmail.com>
2024-01-19 11:55:36 -0500

    :truck: [Utilities] Relocate error-message from helpers

--- pkg/utilities/index.ts
+++ pkg/utilities/index.ts
@@ -1,3 +1,4 @@
 export {arrayOfLength, arrayShallowEquals} from './array';
+export {getErrorMessage} from './error';
 export {assertNumber, clamp, progressPercentage} from './numbers';
 export {msToSec, secToMs} from './time';
