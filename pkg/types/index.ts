40cc3dcd14661fab0e62474be2ed06a05011d13e
beefchimi <curtdulmage@gmail.com>
2024-01-19 15:03:36 -0500

    :label: [Types] Add more global types

--- pkg/types/index.ts
+++ pkg/types/index.ts
@@ -1,4 +1,4 @@
-export type PrimitiveType =
+export type Primitive =
   | string
   | number
   | bigint
@@ -7,6 +7,23 @@ export type PrimitiveType =
   | undefined
   | null;
 
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Builtin = Primitive | Function | Date | Error | RegExp;
+
+export type AnyFn = (...args: any[]) => any;
+export type Fn = () => void;
+
+export type GlobalEventCallback = (event: any) => void;
+export type GlobalEventTarget =
+  | Document
+  | Window
+  | HTMLElement
+  | null
+  | undefined;
+
+export type UtcMilliseconds = ReturnType<typeof Date.now>;
+
+export type IntervalId = number | ReturnType<typeof setInterval>;
 export type TimeoutId = number | ReturnType<typeof setTimeout>;
 
 // Tuple: custom error, original error.
