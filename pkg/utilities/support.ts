d3bfb62c61287ad29e99100d48e9ea6c050da680
beefchimi <curtdulmage@gmail.com>
2024-04-27 19:57:44 -0400

    :fire: [Utilities] Remove local package in favour of beeftools

--- pkg/utilities/support.ts
+++
@@ -1,9 +0,0 @@
-export function supportDom() {
-  return typeof window?.document?.createElement !== 'undefined';
-}
-
-export function supportMatchMedia() {
-  return (
-    window && 'matchMedia' in window && typeof window.matchMedia === 'function'
-  );
-}
