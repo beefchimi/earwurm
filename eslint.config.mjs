05a4c482158912ccac2b8d69c17bcae8c4efb705
beefchimi <curtdulmage@gmail.com>
2024-05-30 16:16:38 -0400

    :hammer_and_wrench: [Eslint] Try configuring root eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,29 @@
-{
-  "env": {
-    "browser": true,
-    "es2022": true,
+import globals from 'globals';
+import configLove from 'eslint-config-love';
+// Includes both `config` and `plugin`.
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['.nx/cache', 'coverage/**', 'dist/**', 'scrap/**'],
   },
-  "extends": ["love", "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+  configLove,
+  pluginPrettier,
+  {
+    name: 'root-rules',
+    files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+    languageOptions: {
+      ...configLove.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+    },
   },
-  "rules": {
-    "no-console": "error",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-  },
-}
+];
