7155d749f5c2e1bb7d05041636067ea057ba12f7
beefchimi <curtdulmage@gmail.com>
2024-07-30 12:10:12 -0400

    :art: [Format] All helpers folder files

--- pkg/helpers/fetch-audio-buffer.ts
+++ pkg/helpers/fetch-audio-buffer.ts
@@ -1,8 +1,12 @@
 async function fetchRequest(path: string, options?: RequestInit) {
   const audioRequest = new Request(path, options);
 
+  // eslint-disable-next-line ts/return-await
   return await fetch(audioRequest).then((response) => {
-    if (!response.ok) throw new Error(`Network status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`Network status: ${response.status}`);
+    }
+
     return response;
   });
 }
@@ -12,7 +16,10 @@ export async function fetchAudioBuffer(
   context: AudioContext,
   options?: RequestInit,
 ) {
+  // eslint-disable-next-line ts/return-await
   return await fetchRequest(path, options)
+    // eslint-disable-next-line ts/return-await
     .then(async (response) => await response.arrayBuffer())
+    // eslint-disable-next-line ts/return-await
     .then(async (arrayBuffer) => await context.decodeAudioData(arrayBuffer));
 }
--- pkg/helpers/tests/unlock-audio-context.test.ts
+++ pkg/helpers/tests/unlock-audio-context.test.ts
@@ -1,4 +1,4 @@
-import {afterEach, describe, it, expect, vi} from 'vitest';
+import {afterEach, describe, expect, it, vi} from 'vitest';
 import {audioBufferSourceNodeEndedEvent} from '@earwurm/mocks';
 
 import {unlockAudioContext} from '../unlock-audio-context';
