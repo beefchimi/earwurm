2e63e0a4032423b3331bb47074a8b9c0ef106402
beefchimi <curtdulmage@gmail.com>
2024-07-30 19:22:45 -0400

    :art: [Format] Websites helpers folder

--- app/website/src/helpers/vue.ts
+++ app/website/src/helpers/vue.ts
@@ -1,10 +1,11 @@
 import {getCurrentScope, onScopeDispose, unref} from 'vue';
-import type {AnyFn, Fn} from '@earwurm/types';
+import type {AnyFn, Fn} from 'beeftools';
 import type {MaybeRefOrGetter} from '@/types';
 
 // Adapted from Vue Use: https://vueuse.org/
 
 export function toValue<T>(something: MaybeRefOrGetter<T>): T {
+  // eslint-disable-next-line ts/no-unsafe-return
   return typeof something === 'function'
     ? (something as AnyFn)()
     : unref(something);
